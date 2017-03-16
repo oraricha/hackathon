f2e54e2d595e161ec360a599f73d350feac31917
oraricha <orarich@gmail.com>
2017-03-16 08:42:50 +0200

    feat(video stream recording): added recording of stream into file

--- src/components/VideoRecorder/VideoRecorder.js
+++ src/components/VideoRecorder/VideoRecorder.js
@@ -1,58 +1,113 @@
 import React, {Component, PropTypes} from 'react';
+import RecordRTC from 'recordrtc';
 
 class VideoPlayer extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      isRecording: false
+      isRecording: false,
+      recordVideo: null,
+      uploading: false,
+      uploadSuccess: null
     };
+
+    this.startRecord = this.startRecord.bind(this);
+    this.stopRecord = this.stopRecord.bind(this);
   }
 
-  updatePlayerState() {
-    const player = this.refs.player;
-    if (!player) {
-      return;
-    }
+  // updatePlayerState() {
+  //   const player = this.refs.player;
+  //   if (!player) {
+  //     return;
+  //   }
+  //
+  //   if (player.readyState === 0) {
+  //     setTimeout(function() { // eslint-disable-line
+  //       this.updatePlayerState();
+  //     }.bind(this), 1000);
+  //     return;
+  //   }
+  //
+  //   player.load();
+  //   player.pause();
+  //
+  //   if (this.props.autoPlay && player.paused) {
+  //     player.play();
+  //   }
+  // }
 
-    if (player.readyState === 0) {
-      setTimeout(function() { // eslint-disable-line
-        this.updatePlayerState();
-      }.bind(this), 1000);
-      return;
-    }
-
-    player.load();
-    player.pause();
-
-    if (this.props.autoPlay && player.paused) {
-      player.play();
-    }
+  startRecord(stream) {
+    this.setState({
+      isRecording: true,
+      recordVideo: RecordRTC(stream, { type: 'video' })
+    });
+    // this.state.recordVideo = RecordRTC(stream, { type: 'video' });
+    this.state.recordVideo.startRecording();
   }
 
-  recordSuccessCb(stream) {
+  stopRecord() {
+    this.setState({isRecording: false});
+    this.state.recordVideo.stopRecording(() => {
+      const recorder = this.refs.recorder;
+      // let params = {
+      //   type: 'video/webm',
+      //   data: this.state.recordVideo.blob,
+      //   id: Math.floor(Math.random() * 90000) + 10000
+      // };
+
+      // recorder.src = this.state.recordVideo.blob;
+      const videoFile = this.state.recordVideo.blob;
+      // this.setState({ uploading: true });
+
+      // S3Upload(params)
+      //   .then((success) => {
+      //     console.log('enter then statement');
+      //     if(success) {
+      //       console.log(success)
+      //       this.setState({ uploadSuccess: true, uploading: false });
+      //     }
+      //   }, (error) => {
+      //     alert(error, 'error occurred. check your aws settings and try again.');
+      //   });
+    });
+  }
+
+  streamSuccessCb(stream) {
+    // stream the video
     const recorder = this.refs.recorder;
     const source = window.webkitURL.createObjectURL(stream);
     recorder.autoplay = true;
     recorder.src = source;
-    this.setState({isRecording: true});
     console.log(stream);
+
+    // record the video
+    this.startRecord(stream);
+    // this.setState({isRecording: true});
+
+    // stop recording after 7 sec
+    setTimeout(() => {
+      if (this.state.isRecording) {
+        this.stopRecord();
+      }
+    }, 7 * 1000);
   }
 
-  recordFailCb(err) {
+  streamFailCb(err) {
     console.log(err);
     if (err.code === 1) {
       alert('You can click the button again anytime to enable.');
     }
   }
 
-  record() {
-    if (this.state.isRecording) {}
-    else {
+  stream() {
+    if (this.state.isRecording) {
+      this.stopRecord();
+    } else {
       navigator.webkitGetUserMedia({
         video: {width: 280, height: 720},
         audio: true
-      }, this.recordSuccessCb.bind(this), this.recordFailCb.bind(this));
+      }, this.streamSuccessCb.bind(this), this.streamFailCb.bind(this));
     }
   }
 
@@ -60,14 +115,14 @@ class VideoPlayer extends Component {
     // const {url} = this.props;
     const {isRecording} = this.state;
     const styles = require('./VideoRecorder.scss');
-    this.updatePlayerState();
+    // this.updatePlayerState();
     return (
       <div>
         <video ref="recorder" style={{width: '100%'}}
                src="">
         </video>
 
-        <button className={`${styles['record-btn']} ${isRecording ? styles.active : ''}`} onClick={this.record.bind(this)}/>
+        <button className={`${styles['record-btn']} ${isRecording ? styles.active : ''}`} onClick={this.stream.bind(this)}/>
       </div>
     );
   }
